6257752e9d0e05e75efd9527a26ff9219c82a9a0
Andreas Nicolai <andreas.nicolai@gmx.net>
2025-06-19 07:54:24 +0200

    Added test example for weeding curve fitter.

---
+++ code/WeedingCurveFitter/WeedingCurveFitter.pro
@@ -0,0 +1,37 @@
+TARGET   = WeedingCurveFitter
+QT       += core gui widgets
+CONFIG   += c++11
+
+c++11 {
+	CONFIG           += strict_c++
+}
+
+win32 {
+	# Pfad zu den Qwt Headerdateien hinzufügen
+	INCLUDEPATH += C:/qwt-6.3.0/include
+	CONFIG(debug, debug|release) {
+		QWTLIB = qwtd
+	}
+	else {
+		QWTLIB = qwt
+	}
+	# Linkerpfad
+	LIBS += -LC://qwt-6.3.0/lib -l$$QWTLIB
+}
+else {
+	lessThan(QT_MAJOR_VERSION, 5) {
+		# Pfad zu den Qwt Headerdateien hinzufügen
+		INCLUDEPATH += /usr/local/qwt-6.3.0/include/
+		# Linkerpfad, unter Linux wird standardmäßig nur die release-Version der Lib gebaut und installiert
+		LIBS += -L/usr/local/qwt-6.3.0/lib -lqwt
+	}
+	else {
+		INCLUDEPATH += /home/ghorwin/git/_webpages/qwtbook/code/qwt-6.3.0/src \
+			/home/ghorwin/git/_webpages/qwtbook/code/qwt-6.3.0/classincludes/
+		LIBS += -L/home/ghorwin/git/_webpages/qwtbook/code/qwt-6.3.0/build/Desktop-Debug/lib -lqwt
+	}
+}
+
+
+SOURCES += \
+	main.cpp
---
+++ code/WeedingCurveFitter/main.cpp
@@ -0,0 +1,188 @@
+/*
+
+The MIT License (MIT)
+
+Copyright (c) 2025 Andreas Nicolai
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+*/
+
+#include <QApplication>
+#include <QPen>
+#include <QFile>
+#include <QTextStream>
+#include <QElapsedTimer>
+#include <QDebug>
+
+#include <cmath>
+
+#define ORIGINAL_HEADERS
+#ifndef ORIGINAL_HEADERS
+
+#include <QwtPlot>
+#include <QwtPlotCurve>
+#include <QwtLegend>
+#include <QwtText>
+#include <QwtPlotGrid>
+#include <QwtLogScaleEngine>
+#include <QwtPlotMarker>
+#include <QwtPlotZoomer>
+#include <QwtPlotPanner>
+#include <QwtPlotLayout>
+#include <QwtScaleDraw>
+#include <QwtCurveFitter>
+#else
+
+#include <qwt_plot.h>
+#include <qwt_plot_curve.h>
+#include <qwt_legend.h>
+#include <qwt_text.h>
+#include <qwt_plot_grid.h>
+#include <qwt_plot_marker.h>
+#include <qwt_plot_zoomer.h>
+#include <qwt_plot_panner.h>
+#include <qwt_scale_engine.h>
+#include <qwt_scale_draw.h>
+#include <qwt_plot_layout.h>
+#include <qwt_curve_fitter.h>
+#include <qwt_polar_fitter.h>
+#include <qwt_weeding_curve_fitter.h>
+#include <qwt_spline_curve_fitter.h>
+#include <qwt_spline_pleasing.h>
+#include <qwt_weeding_curve_fitter.h>
+#include <qwt_symbol.h>
+#include <qwt_plot_canvas.h>
+#endif
+
+class BenchmarkedPlotCanvas : public QwtPlotCanvas {
+public:
+
+	// QWidget interface
+protected:
+	void paintEvent(QPaintEvent * event) override {
+		QElapsedTimer timer;
+		timer.start();
+		QwtPlotCanvas::paintEvent(event);
+		qDebug() << "paintEvent() : " << timer.elapsed() << "ms";
+	}
+};
+
+
+class BenchmarkedWeedingCurveFitter : public QwtWeedingCurveFitter {
+public:
+	QPolygonF fitCurve(const QPolygonF & polygon) const override {
+		QElapsedTimer timer;
+		timer.start();
+		const QPolygonF & stripped = QwtWeedingCurveFitter::fitCurve(polygon);
+		qDebug() << "fitCurve()->" << stripped.count() << "points, " << timer.elapsed() << "ms";
+		return stripped;
+	}
+};
+
+
+int main(int argc, char *argv[]) {
+	QApplication a(argc, argv);
+	QwtPlot plot;
+	plot.resize(600,400);
+
+	// etwas Abstand zwischen Rand und Achsentiteln
+	plot.setContentsMargins(8,8,8,8);
+	// Hintergrund der Zeichenfläche soll weiß sein
+	plot.setCanvas(new BenchmarkedPlotCanvas);
+	plot.setCanvasBackground( Qt::white );
+
+	// Daten zum Darstellen generieren
+	QVector<double> x, y;
+	for (unsigned int i=0; i<10000000; ++i) {
+		x.append(i);
+		y.append(std::sin(i*0.00001));
+	}
+
+	QwtPlotCurve *curve = new QwtPlotCurve();
+	curve->setPen(QColor(180,40,20), 1);
+	// curve->setBrush(QColor(180,40,20));
+	curve->setTitle("Gamma-Spektrum");
+	curve->setRenderHint( QwtPlotItem::RenderAntialiased, true); // Antialiasing verwenden
+	curve->setSamples(x, y);
+	curve->attach(&plot); // Plot takes ownership
+
+	QwtWeedingCurveFitter * weedingFitter = new BenchmarkedWeedingCurveFitter;
+	weedingFitter->setTolerance(1);
+	// weedingFitter->setChunkSize(50000);
+	curve->setCurveFitter(weedingFitter);
+	curve->setCurveAttribute(QwtPlotCurve::Fitted, true);
+
+	// QwtSymbol * symbol = new QwtSymbol(QwtSymbol::Ellipse);
+	// symbol->setSize(4);
+	// curve->setSymbol(symbol);
+
+	// Legende anzeigen
+	QwtLegend * legend = new QwtLegend();
+	QFont legendFont;
+	legendFont.setPointSize(8);
+	legend->setFont(legendFont);
+	plot.insertLegend( legend , QwtPlot::BottomLegend); // plot takes ownership
+
+	// Haupt- und Nebengitter anzeigen
+	QwtPlotGrid *grid = new QwtPlotGrid();
+	QPen gridPen(Qt::gray);
+	gridPen.setStyle(Qt::DashLine);
+	grid->setMajorPen(gridPen);
+	// Minor grid
+	grid->enableYMin( true );
+	gridPen.setColor(Qt::lightGray);
+	gridPen.setStyle(Qt::DotLine);
+	grid->setMinorPen(gridPen);
+	grid->attach( &plot ); // plot takes ownership
+
+
+	// Achsen formatieren
+	QFont axisFont;
+	axisFont.setPointSize(8);
+	axisFont.setBold(true);
+	QFont axisLabelFont;
+	axisLabelFont.setPointSize(8);
+	// X-Achse
+	QwtText axisTitle("Kanal");
+	axisTitle.setFont(axisFont);
+	// Titel Text und Font setzen
+	plot.setAxisTitle(QwtPlot::xBottom, axisTitle);
+	// Font für Achsenzahlen setzen
+	plot.setAxisFont(QwtPlot::xBottom, axisLabelFont);
+	// Y-Achse
+	axisTitle.setText("Ereignisse");
+	plot.setAxisTitle(QwtPlot::yLeft, axisTitle);
+	plot.setAxisFont(QwtPlot::yLeft, axisLabelFont);
+
+	// Zoomer hinzufügen
+	// Achtung: NICHT QwtPlot selbst als 3 Argument übergeben, sonder das canvas()
+	QwtPlotZoomer * zoomer = new QwtPlotZoomer(QwtPlot::xBottom, QwtPlot::yLeft, plot.canvas());  // plot takes ownership
+	zoomer->setTrackerMode( QwtPlotPicker::AlwaysOn ); // Kurvenvwerte unterm Cursor anzeigen
+
+	// Panner hinzufügen, wie auch beim PlotZoomer muss das Canvas-Objekt als Argument übergeben werden
+	QwtPlotPanner * panner = new QwtPlotPanner(plot.canvas());  // plot takes ownership
+	panner->setMouseButton(Qt::MiddleButton); // Mittlere Maustaste verschiebt
+	plot.show();
+
+
+
+
+	return a.exec();
+}
