56379c051b7ac22f38f08226ea6eedadf5881320
Andreas Nicolai <andreas.nicolai@gmx.net>
2026-05-04 13:51:09 +0200

    New version 2.1 released

--- book/de/build.sh
+++ book/de/build.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
 
 echo '*** Generating html ***' &&
-asciidoctor QwtBook.adoc &&
+make html &&
 
 echo '*** Generating pdf ***' &&
-asciidoctor-pdf -a pdf-theme=pdf-theme.yml -r ../rouge_theme.rb  -a pdf-fontsdir="../fonts;GEM_FONTS_DIR" QwtBook.adoc &&
-#asciidoctor-pdf -a pdf-theme=pdf-theme.yml -r ./rouge_theme.rb -r asciidoctor-mathematical -a pdf-fontsdir="./fonts;GEM_FONTS_DIR" QwtBook.adoc
+make pdf &&
 
 echo '*** Copying files to ../docs directory' &&
-
-mkdir ../../docs/de
-cp QwtBook.html ../../docs/de/index.html &&
-mv QwtBook.pdf ../../docs/QwtBook_de.pdf &&
-
-cp -r ../images ../../docs/ &&
-cp -r ../fonts ../../docs/ &&
-cp -r ../css ../../docs/ &&
-cp -r ../code ../../docs/
-
+make deploy
